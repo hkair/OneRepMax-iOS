4ac6ffc563a3767b5f8be50f60d808902cff9754
hkair <hb2kang@gmail.com>
2020-10-12 16:29:36 -0400

    Initial Commit

---
+++ OneRepMax-Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneRepMax-Calculator
+//
+//  Created by Hobin Kang on 2020-10-12.
+//  Copyright © 2020 Hobin Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
